65c3b627dc9163412ff56c87d096679315229c8d
Petr Pechkurov <petr.pechkurov@gmail.com>
2022-07-19 15:22:44 +0300

    Updated run method

--- src/run.ts
+++ src/run.ts
@@ -1,8 +1,13 @@
+import express from 'express';
 import { App } from './app';
 import { LoggerService } from './services/logger.service';
+import { UsersController } from './users/users.controller';
 
 function run(port: number) {
-  const app = new App(port, new LoggerService());
+  const logger = new LoggerService();
+
+  const app = new App(express(), port, logger, new UsersController(logger));
+
   app.init();
 }
 
